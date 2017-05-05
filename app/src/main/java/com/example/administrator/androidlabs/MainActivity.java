7678a374ea907cee438ff0a2dc50b344b3a04751
michaelhyun <michael.hyun3@gmail.com>
2017-05-04 18:56:05 -0700

    Initial commit

---
+++ app/src/main/java/com/example/administrator/androidlabs/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.androidlabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by administrator on 5/4/17.
+ */
+
+public class MainActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
